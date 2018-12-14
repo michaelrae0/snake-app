dd17bdf86650c6597f2c4f615a6147e82e1e06d2
mdrae <mdrae0@gmail.com>
2018-12-14 10:29:06 -0600

    Updates

---
+++ precache-manifest.b3666669a85e9942809d8ea170348a47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c111981a3c40ce1a6db",
+    "url": "/snake-app/static/css/main.4ea050c0.chunk.css"
+  },
+  {
+    "revision": "3c111981a3c40ce1a6db",
+    "url": "/snake-app/static/js/main.3c111981.chunk.js"
+  },
+  {
+    "revision": "b61f9f0b70fcc34a85f9",
+    "url": "/snake-app/static/js/1.b61f9f0b.chunk.js"
+  },
+  {
+    "revision": "85f931246765374a984e",
+    "url": "/snake-app/static/js/runtime~main.85f93124.js"
+  },
+  {
+    "revision": "3e39330815a79bf2cc9cb573dcf67f13",
+    "url": "/snake-app/index.html"
+  }
+];
